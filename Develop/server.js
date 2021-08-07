8885df3db52c5f78e97f1fd6d228741293a8010d
Blaine Curren <blaine.curren@gmail.com>
2021-08-07 08:10:39 -0500

    added findAll route

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 // import sequelize connection
 
 const app = express();
@@ -13,4 +13,9 @@ app.use(routes);
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+});
+
+// sync sequelize models to the database
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
